3390d971e0585ce68a0df8fb8b297f9b8ace74b9
Abhishek Madhu <abhishek.madhu@stratavar.com>
2020-08-02 17:01:22 +0530

    universalize CORS 5

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "chrome-search://local-ntp"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
