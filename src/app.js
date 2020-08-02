ef0e692af2f8aa642e17ec0c673861f4327294cf
Abhishek Madhu <abhishek.madhu@stratavar.com>
2020-08-02 22:34:47 +0530

    Suvradeep localhost ngport removed

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost:4200"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Origin", "chrome-search://local-ntp"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Origin", "localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "chrome-search://local-ntp"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
