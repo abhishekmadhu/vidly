a16bfdae5a8e477ea6567e794df612d54446671b
Abhishek Madhu <abhishek.madhu@stratavar.com>
2020-08-02 16:52:24 +0530

    universalize CORS 2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,12 @@ const cors = require('cors');
 // Create an express application 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*.*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/database')();
